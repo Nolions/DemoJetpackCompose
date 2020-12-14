7f5e0660dea9db74c155ba319551ce3c9ca5ea28
NolionsChen <nolionschen@jetfuel.com.tw>
2020-12-14 17:57:35 +0800

    Basic knowledge

--- app/src/main/java/com/example/demojetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/demojetpackcompose/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.demojetpackcompose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.demojetpackcompose.ui.DemoJetpackComposeTheme
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +28,7 @@ class MainActivity : AppCompatActivity() {
             DemoJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    NewsStory()
                 }
             }
         }
@@ -29,10 +40,39 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun NewsStory() {
+    val image = imageResource(R.drawable.demo)
+
+    Column(modifier = Modifier.padding(20.dp)) {
+        val imageModifier = Modifier
+            .preferredHeight(180.dp)
+            .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(20.dp))
+
+        Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
+
+        Spacer(Modifier.preferredHeight(10.dp))
+
+        Text(
+            "A day wandering through the sandhills " +
+                    "in Shark Fin Cove, and a few of the " +
+                    "sights I saw",
+            style = typography.h6,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        Text("Davenport, California", style = typography.body2)
+        Text("December 2018", style = typography.body2)
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DemoJetpackComposeTheme {
-        Greeting("Android")
+        NewsStory()
     }
 }
